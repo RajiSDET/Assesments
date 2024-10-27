912f88ee239aa110bc8e1d5c92f1a76fe7b9ed73
RajiSDET <rajalakshmisdet@gmail.com>
2024-10-27 18:19:22 +0530

    RAJI_Assestment

--- src/main/java/Sprint_01/Assesment_Oct27.java
+++ src/main/java/Sprint_01/Assesment_Oct27.java
@@ -1,25 +1,46 @@
 package Sprint_01;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Assesment_Oct27 {
 
-	public static void main(String args[]) throws InterruptedException {
+	public static void main(String args[]) throws InterruptedException, MalformedURLException {
 
-	ChromeOptions options = new ChromeOptions();
+	/*ChromeOptions options = new ChromeOptions();
 		options.addArguments("--disable-notifications"); // Disable pop-ups and notifications
 		options.addArguments("--disable-popup-blocking"); // Disable blocking for pop-ups
 
-		ChromeDriver driver = new ChromeDriver(options);
+		ChromeDriver driver = new ChromeDriver(options);*/
+		
+		// Set Remote WebDriver and Desired Capabilities for GRID Execution
+    	// UI Console - http://20.40.48.160:4444/ui/#/sessions - click on Video - secret is the password
+    	
+    	// Set ChromeOptions
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+         
+    	DesiredCapabilities dc = new DesiredCapabilities();
+    			dc.setBrowserName("chrome");
+    			dc.setPlatform(Platform.LINUX);
+    			dc.setCapability(ChromeOptions.CAPABILITY, options);
+    		  //dc.merge(options);
+    			RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.40.48.160:4444/wd/hub"), dc);
+
+           
 
 		// Step 1: Login to Sales-force Application
 		driver.manage().deleteAllCookies();
