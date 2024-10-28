3abf409ae92379d3fac6f532530c020ab20d7188
RajiSDET <rajalakshmisdet@gmail.com>
2024-10-28 19:21:12 +0530

    RAJI_Assestment

--- src/main/java/Sprint_01/Assesment_Oct27.java
+++ src/main/java/Sprint_01/Assesment_Oct27.java
@@ -41,82 +41,53 @@ public class Assesment_Oct27 extends BaseClass {
     		  //dc.merge(options);
     			RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.40.48.160:4444/wd/hub"), dc);*/
 
-           
+     // Step 1: Accept cookies
+     driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).click();
+
+     // Step 2: Click on 'See All Player Stats'
+     driver.findElement(By.xpath("//a[text()='See All Player Stats']")).click();
+
+     // Step 3: Choose 'Season' as '2023-24'
+     Select seasonDropdown = new Select(driver.findElement(By.xpath("(//select[@class='DropDown_select__4pIg9'])[1]")));
+     seasonDropdown.selectByVisibleText("2023-24");
+
+     // Step 4: Choose 'Season Type' as 'NBA Cup'
+     Select seasonTypeDropdown = new Select(driver.findElement(By.xpath("(//select[@class='DropDown_select__4pIg9'])[2]")));
+     seasonTypeDropdown.selectByVisibleText("NBA Cup");
+
+     // Step 5: Choose 'Per Mode' as 'Per Game'
+     Select perModeDropdown = new Select(driver.findElement(By.xpath("(//select[@class='DropDown_select__4pIg9'])[3]")));
+     perModeDropdown.selectByVisibleText("Per Game");
+
+     // Step 6: Choose 'Season Segment' as 'Last Game'
+     Select seasonSegmentDropdown = new Select(driver.findElement(By.xpath("//select[@class='SplitComboDropDown_select__G7wsG']")));
+     seasonSegmentDropdown.selectByValue("LastNGames=1");
+
+     // Remove unwanted elements
+     driver.executeScript("document.querySelectorAll('.bx-align, .bx-shroud, .ad-class-name').forEach(el => el.remove());");
+
+     // Scroll down the page
+     driver.executeScript("window.scrollBy(0, 500)");
+
+     // Step 7: Click on the player name with the lowest age
+     driver.findElement(By.xpath("//table[@class='Crom_table__p1iZz']//th[4]")).click();
+
+     // Step 8: Click on the player's profile
+     driver.findElement(By.xpath("(//tbody[@class='Crom_body__UYOcU']//td[2])[1]")).click();
+
+     // Step 9: Get the experience of the player
+     String experienceText = driver.findElement(By.xpath("//p[text()='EXPERIENCE']/following-sibling::p")).getText();
+     System.out.println(experienceText);
+
+     // Step 10: Verify the player's experience is 1 year
+     if (experienceText.equals("1 Year")) {
+         System.out.println("Test case Passed");
+     } else {
+         System.out.println("Test case Failed");
+     }    
 
 		
 
-		driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).click();
-
-		// Step 2. Click on 'See All Player Stats'
-		driver.findElement(By.xpath("//a[text()='See All Player Stats']")).click();
-
-		/*WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement fileElement = wait.until(ExpectedConditions
-				.elementToBeClickable(By.xpath("(//div[@class='bx-wrap']/button[@data-click='close'])[2]")));
-		fileElement.click();*/
 		
-		
-		// Step 3. Choose 'Season' as '2023-24'
-
-		WebElement dropdownElement = driver.findElement(By.xpath("(//select[@class='DropDown_select__4pIg9'])[1]"));
-		Select dropdown = new Select(dropdownElement);
-		dropdown.selectByVisibleText("2023-24");
-
-		// Step 4. Choose 'Season Type' as 'NBA Cup'
-		WebElement seasonTypeDropdown = driver.findElement(By.xpath("(//select[@class='DropDown_select__4pIg9'])[2]"));
-		Select seasonTypeSelect = new Select(seasonTypeDropdown);
-		seasonTypeSelect.selectByVisibleText("NBA Cup");
-
-		// Step 5. Choose 'Per Mode' as 'Per Game'
-		WebElement perModeDropdown = driver.findElement(By.xpath("(//select[@class='DropDown_select__4pIg9'])[3]"));
-		Select perModeSelect = new Select(perModeDropdown);
-		perModeSelect.selectByVisibleText("Per Game");
-
-		// Step 6. Choose 'Season Segment' as 'Last Game'
-		WebElement seasonSegmentDropdown = driver
-				.findElement(By.xpath("//select[@class='SplitComboDropDown_select__G7wsG']"));
-		Select seasonSelect = new Select(seasonSegmentDropdown);
-		seasonSelect.selectByValue("LastNGames=1");
-
-		
-		// WebElement CloseBanner =
-		// wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='bx-wrap']//span[text()='close
-		// email sign up banner']")));
-		// CloseBanner.click();
-
-		//driver.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-		
-		int pixelsToScroll = 500;
-		driver.executeScript("window.scrollBy(0, " + pixelsToScroll + ")");
-
-		Thread.sleep(10);
-		// Step 7. Click on the player name with lowest age
-		driver.findElement(By.xpath("//table[@class='Crom_table__p1iZz']//th[4]")).click();
-
-		
-		//driver.findElement(By.xpath("//th[@field='AGE']")).click();
-		
-		WebElement closeBanner = driver
-				.findElement(By.xpath("(//div[@class='bx-wrap']/button[@data-click=\"close\"])[1]"));
-		driver.executeScript("arguments[0].click();", closeBanner);
-
-		// Step 8. Click on the Profile
-		driver.findElement(By.xpath("(//tbody[@class=\"Crom_body__UYOcU\"]//td[2])[1]")).click();
-
-		// Step 9. Get the Experience of the player
-		
-		/*WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement fileElement = wait.until(ExpectedConditions
-				.elementToBeClickable(By.xpath("(//div[@class='bx-wrap']/button[@data-click='close'])[2]")));
-		fileElement.click();*/
-		// Step 10. Verify the player experience as 1.
-		
-		String Exp = driver.findElement(By.xpath("//p[text()='EXPERIENCE']/following-sibling::p")).getText();
-		System.out.println(Exp);
-		
-		if(Exp.equals("1 Year")) {
-			System.out.println("Test case Passed");
-		}
-
 	}
 }
